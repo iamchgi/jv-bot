5597c384377ae082f09efef52bb69eb73a79dfbc
Hryhorii Chernolutskyi <iamchgi1976@gmail.com>
2023-03-03 13:13:30 +0200

    initial commit

---
+++ src/main/java/ua/mario/jvbot/JvBotApplication.java
@@ -0,0 +1,13 @@
+package ua.mario.jvbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvBotApplication.class, args);
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/ua/mario/jvbot/JvBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.mario.jvbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
